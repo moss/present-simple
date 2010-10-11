7e0073d2cb7e4cb4a7681990e5e664c152051bce
Moss Collum <moss.collum@gmail.com>
2010-10-11 18:22:28 -0400

    A bit more testing around BeforeAdvice

--- src/test/java/net/m14m/presentsimple/advice/BeforeAdviceTest.java
+++ src/test/java/net/m14m/presentsimple/advice/BeforeAdviceTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class BeforeAdviceTest {
@@ -22,7 +23,23 @@ public class BeforeAdviceTest {
 
         advice.advise(new NamedMethodInvocation(this, "myMethod", 5));
 
-        assertEquals(Arrays.asList("before", "number 5"), log);
+        assertEquals("should have run interceptor and method", Arrays.asList("before", "number 5"), log);
+    }
+
+    @Test public void passesInInvocationDetails() throws Throwable {
+        final Method expectedMethod = getClass().getMethod("myMethod", Integer.class);
+        final Integer expectedArgument = 5;
+        final Object expectedReceiver = this;
+
+        BeforeAdvice advice = new BeforeAdvice() {
+            public void before(Method method, Object[] arguments, Object receiver) {
+                assertEquals("method", expectedMethod, method);
+                assertArrayEquals("arguments", new Object[]{expectedArgument}, arguments);
+                assertEquals("receiver", expectedReceiver, receiver);
+            }
+        };
+
+        advice.advise(new NamedMethodInvocation(this, "myMethod", expectedArgument));
     }
 
     public String myMethod(Integer number) {
