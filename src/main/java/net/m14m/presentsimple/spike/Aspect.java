7f9c31d9844a10575798ebef5deefbdaa894cbae
Moss Collum <moss.collum@gmail.com>
2010-10-11 11:04:42 -0400

    Deleted the spike version, now that the real version has passed it.

--- src/main/java/net/m14m/presentsimple/spike/Aspect.java
+++
@@ -1,9 +0,0 @@
-package net.m14m.presentsimple.spike;
-
-import java.lang.annotation.Annotation;
-
-public interface Aspect {
-    Class<? extends Annotation> getAnnotation();
-
-    Object intercept(MethodInvocation invocation) throws Throwable;
-}
--- src/main/java/net/m14m/presentsimple/spike/MethodInvocation.java
+++
@@ -1,9 +0,0 @@
-package net.m14m.presentsimple.spike;
-
-import java.lang.reflect.Method;
-
-public interface MethodInvocation {
-    public Object invoke() throws Throwable;
-
-    Method getMethod();
-}
