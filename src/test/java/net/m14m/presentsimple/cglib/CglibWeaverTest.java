3b2bbba21073aa8804feeb2043981f03acd28d6d
Moss Collum <moss.collum@gmail.com>
2010-10-22 20:28:08 -0400

    Also, it creates a decorated class with appropriate constructors.

--- src/test/java/net/m14m/presentsimple/cglib/CglibWeaverTest.java
+++ src/test/java/net/m14m/presentsimple/cglib/CglibWeaverTest.java
@@ -13,30 +13,39 @@ import static org.junit.Assert.assertEquals;
 
 
 public class CglibWeaverTest {
-    private SampleClass instance;
+    private CglibWeaver weaver;
 
     @Before public void setUpWeaver() {
-        CglibWeaver weaver = new CglibWeaver();
+        weaver = new CglibWeaver();
         weaver.register(new LoggingDecorator());
         weaver.register(new TransactionalDecorator());
-        instance = weaver.createInstance(SampleClass.class);
     }
 
     @Test public void shouldDecorateAnnotatedMethods() {
+        SampleClass instance = weaver.createInstance(SampleClass.class);
         assertEquals("Logged(contactServer())", instance.contactServer());
         assertEquals("Transactional(saveChanges())", instance.saveChanges());
     }
 
     @Test public void shouldAllowMultipleAnnotationsOnAMethod() {
+        SampleClass instance = weaver.createInstance(SampleClass.class);
         assertEquals("Transactional(Logged(clearDatabase()))", instance.clearDatabase());
     }
 
     @Test public void shouldNotAffectMethodsWithoutAnnotations() {
+        SampleClass instance = weaver.createInstance(SampleClass.class);
         assertEquals("sayHello()", instance.sayHello());
     }
 
     @Test public void shouldDecorateInternalMethodCalls() {
+        SampleClass instance = weaver.createInstance(SampleClass.class);
         assertEquals("Transactional(saveChanges()) Transactional(Logged(clearDatabase()))", instance.saveAndClear());
+    }
+
+    @Test public void shouldCreateClassWithConstructorMatchingSuper() throws Exception {
+        Class<? extends NoDefaultConstructor> decoratedClass = weaver.decorateClass(NoDefaultConstructor.class);
+        NoDefaultConstructor instance = decoratedClass.getConstructor(String.class).newInstance("foo");
+        assertEquals("Logged(constructor parameter: foo)", instance.showConstructorParameter());
     }
 
     @Retention(RetentionPolicy.RUNTIME)
@@ -75,4 +84,15 @@ public class CglibWeaverTest {
         public String sayHello() { return "sayHello()"; }
     }
 
+    public static class NoDefaultConstructor {
+        private String constructorParameter;
+
+        public NoDefaultConstructor(String constructorParameter) {
+            this.constructorParameter = constructorParameter;
+        }
+
+        @Logged public String showConstructorParameter() {
+            return "constructor parameter: " + constructorParameter;
+        }
+    }
 }
